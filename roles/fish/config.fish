c46d753485dcc8fc00f345388ae4434c59c2374e
Levko Ivanchuk <lewkoo@gmail.com>
2020-07-29 22:07:47 +0300

    WIP: Consolidating fish configs among several machines

--- roles/fish/config.fish
+++ roles/fish/config.fish
@@ -1,2 +1,16 @@
+# Load thefuck
+type -q fuck; and thefuck --alias | source 
+
+# Set the theme preferences
 set -g theme_nerd_fonts yes
 set -g theme_powerline_fonts no
+
+# Set the default editor
+export EDITOR='nano -w'
+
+# Virtualenvwrapper
+test -e {$HOME}/.local/bin/virtualenvwrapper.sh; and bass source {$HOME}/.local/bin/virtualenvwrapper.sh
+
+# iter2 shell integration
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
