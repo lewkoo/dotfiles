8f768979e3db46c3f4d6a802267e2199d69a0b9c
Levko Ivanchuk <lewkoo@gmail.com>
2021-02-10 13:55:31 +0200

    Updating the docker aliases

--- roles/docker/docker_aliases.fish
+++ roles/docker/docker_aliases.fish
@@ -5,7 +5,9 @@ abbr -a -g ddf docker system df
 # Stop and remove a single container
 abbr -a -g drm docker rm -f 
 # Stop and remove all running containers
-abbr -a -g drm! docker rm -f (docker ps -a -q)
+function drm!
+  docker rm -f (docker ps -a -q)
+end
 
 abbr -a -g dc docker-compose
 abbr -a -g dcup docker-compose up -d 
@@ -22,6 +24,10 @@ abbr -a -g ds docker-sync
 abbr -a -g dss docker-sync start
 abbr -a -g dsx docker-sync stop
 
+abbr -a -g dsb docker-sync-stack
+abbr -a -g dsbs docker-sync-stack start
+abbr -a -g dsbx docker-sync-stack stop
+
 abbr -a -g dcbomb! docker-compose down -v
 
 # Open bash in a container. Pass docker-compose name
@@ -32,4 +38,8 @@ end
 # Open bash in a container. Pass container name
 function dbash
   docker exec -it $argv /bin/bash
+end
+
+function dir!
+  docker image rm (docker image ls -q)
 end
